514b7b79bd42022b27110ddd9c4d01703f513005
marcelavasquez11 <marcelavax@hotmail.com>
2026-08-11 13:05:09 -0600

    Ajustar aplicación para despliegue

--- MVCPeliculas/Program.cs
+++ MVCPeliculas/Program.cs
@@ -3,31 +3,30 @@ using MVCPeliculas.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<PeliculasDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-using ( var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-   
-        SeedData.Initialize(services);
-   
-}
+// SeedData desactivado temporalmente para el despliegue
+// using (var scope = app.Services.CreateScope())
+// {
+//     var services = scope.ServiceProvider;
+//     SeedData.Initialize(services);
+// }
 
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Home/Error");
-        app.UseHsts();
-    }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles(); // <-- agregar para .NET 8
+app.UseStaticFiles();
 
 app.UseRouting();
 
